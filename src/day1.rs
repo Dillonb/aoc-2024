9395d1040ea96a2a9c53c8796ebbff1a4ec60208
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-12-01 23:19:03 -0800

    initial commit, with day 1

---
+++ src/day1.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn parse_line(line: &str) -> (i32, i32) {
+    let mut split = line.split_whitespace();
+    let left = split.next().unwrap().parse::<i32>().unwrap();
+    let right = split.next().unwrap().parse::<i32>().unwrap();
+    return (left, right);
+}
+
+fn get_input(path: &str) -> (Vec<i32>, Vec<i32>) {
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(parse_line)
+        .unzip();
+
+    left.sort();
+    right.sort();
+
+    return (left, right);
+}
+
+fn main() {
+    let (left, right) = get_input("data/day1/input");
+
+    let sum: i32 = left.clone()
+        .into_iter()
+        .zip(right.clone().into_iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum();
+
+    println!("Part 1, sum: {}", sum);
+
+    let similarity_score: i32 = left.into_iter().map(|item| item * right.clone().into_iter().filter(|right_item| *right_item == item).count() as i32).sum();
+
+    println!("Part 2, similarity score: {}", similarity_score);
+}
